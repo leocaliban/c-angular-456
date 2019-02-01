f4eec16c6e004aad37b1f09e3b6eef70ab67b239
Leocaliban <leocaliban@gmail.com>
2019-02-01 03:23:53 -0300

    Typescript Introdução [Variáveis]

---
+++ 01_typescript/01_primeiro_arquivo/app.ts
@@ -0,0 +1,8 @@
+let mensagem: string = "Teste";
+console.log(mensagem);
+
+let numero: number = 200;
+console.log(numero);
+
+numero += 1;
+console.log(numero);
---
+++ 01_typescript/01_primeiro_arquivo/dist/app.js
@@ -0,0 +1,6 @@
+var mensagem = "Teste";
+console.log(mensagem);
+var numero = 200;
+console.log(numero);
+numero += 1;
+console.log(numero);
