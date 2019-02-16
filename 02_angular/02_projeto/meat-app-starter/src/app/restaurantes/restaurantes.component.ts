5dd37347b78dce797d55cf250e8dc2a837e64c56
Leocaliban <leocaliban@gmail.com>
2019-02-16 00:28:28 -0300

    Aplica animações no campo de busca de restaurantes

--- 02_angular/02_projeto/meat-app-starter/src/app/restaurantes/restaurantes.component.ts
+++ 02_angular/02_projeto/meat-app-starter/src/app/restaurantes/restaurantes.component.ts
@@ -1,12 +1,30 @@
 import { RestauranteService } from './restaurantes.service';
 import { Component, OnInit } from '@angular/core';
 import { Restaurante } from './restaurante/restaurante.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurantes',
-  templateUrl: './restaurantes.component.html'
+  templateUrl: './restaurantes.component.html',
+  animations: [
+    trigger('search-visibility', [
+      state('hidden', style({
+        opacity: 0,
+        'max-height': '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *',
+        animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantesComponent implements OnInit {
+
+  estadoCampoDeBusca = 'hidden';
 
   restaurantes: Restaurante[];
 
@@ -18,6 +36,10 @@ export class RestaurantesComponent implements OnInit {
     this.restauranteService.buscar()
       .subscribe(response =>
         this.restaurantes = response);
+  }
+
+  toggleSearch() {
+    this.estadoCampoDeBusca = this.estadoCampoDeBusca === 'hidden' ? 'visible' : 'hidden';
   }
 
 }
